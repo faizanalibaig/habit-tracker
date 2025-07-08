541c7980f793ac8827e33c891d2cbc66bce2d09c
faizanalibaig <riazfiaz6181@gmail.com>
2025-07-08 20:58:55 +0500

    Image priority and vercel analytics added

--- apps/web/components/home/Header.tsx
+++ apps/web/components/home/Header.tsx
@@ -3,7 +3,13 @@ import Image from 'next/image';
 export default function Header() {
   return (
     <header className='h-20 flex items-center justify-between px-10 flex-shrink-0'>
-      <Image src='/logo.png' alt='Habit Tracker Logo' width={30} height={25} />
+      <Image
+        src='/logo.png'
+        alt='Habit Tracker Logo'
+        width={30}
+        height={25}
+        priority={true}
+      />
       <h2 className='text-base font-sans font-medium text-white'>
         streak - zero days
       </h2>
