455b9b2c4d33f0f689cb80c14091cde1674a06b6
faizanalibaig <riazfiaz6181@gmail.com>
2025-07-09 16:13:55 +0500

    Design issues fixed, streak from localStorage and backend implemented

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -2,7 +2,7 @@ import { WelcomeHeader, WelcomePage } from '../components';
 
 export default function page() {
   return (
-    <section className='h-screen bg-[#2F5249] flex flex-col'>
+    <section className='h-screen-mobile bg-[#2F5249] flex flex-col'>
       <WelcomeHeader />
       <WelcomePage />
     </section>
--- apps/web/components/home/Header.tsx
+++ apps/web/components/home/Header.tsx
@@ -1,8 +1,24 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+import { FaGithubAlt } from 'react-icons/fa';
+
 import Image from 'next/image';
+import Link from 'next/link';
 
 export default function Header() {
+  const [streak, setStreak] = useState<number | null>(0);
+
+  useEffect(() => {
+    const newStreak = parseInt(localStorage.getItem('streak') || '0', 10);
+
+    if (newStreak !== streak) {
+      setStreak(newStreak);
+    }
+  }, [streak]);
+
   return (
-    <header className='h-20 flex items-center justify-between px-10 flex-shrink-0'>
+    <header className='h-20 flex items-center justify-between px-5 sm:px-10 flex-shrink-0'>
       <Image
         src='/logo.png'
         alt='Habit Tracker Logo'
@@ -10,9 +26,38 @@ export default function Header() {
         height={25}
         priority={true}
       />
-      <h2 className='text-base font-sans font-medium text-white'>
-        streak - zero days
-      </h2>
+      <nav className='flex items-center gap-4'>
+        <h2 className='text-sm sm:text-base font-sans font-medium text-white'>
+          streak - {streak || 'zero'} days
+        </h2>
+        <span className='text-white'>{' | '}</span>
+        <Link
+          href='https://github.com/faizanalibaig/habit-tracker'
+          target='_blank'
+        >
+          <FaGithubAlt color='white' size={20} />
+        </Link>
+      </nav>
     </header>
   );
 }
+
+/*Backend*/
+/* Fetch streak from API */
+// const fetchStreak = async () => {
+//   const response = await fetch('api', {
+//     method: 'GET',
+//     headers: {
+//       'Content-Type': 'application/json',
+//     },
+//   });
+
+//   if (!response.ok) {
+//     throw new Error('Failed to fetch streak');
+//   }
+
+//   const data = await response.json();
+//   setStreak(data.streak);
+// };
+
+// fetchStreak();
